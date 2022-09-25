58ae6ae45e7f73ba69ee12473e1c4da70951f3ad
SetKu <78518056+SetKu@users.noreply.github.com>
2022-09-25 17:02:48 -0600

    Added castling tests. Failing atm.

--- Assets/Tests/EditMode/LogicTests.cs
+++ Assets/Tests/EditMode/LogicTests.cs
@@ -44,5 +44,25 @@ namespace Tests {
 
             Assert.AreEqual(originalPositions, newPositions);
         }
+
+        [Test]
+        public void TestCastling() {
+            Board board = new Board();
+
+            Piece king = board.GetKing(Piece.Color.Black);
+            Assert.AreEqual(board.GetLegalMoves(king).Count, 0, "Why does the black king have a move at the start of the game?");
+
+            Move knightMove = new Move(board.PieceAt(new Vector2Int(6, 0)), new Vector2Int(5, 2));
+            board.MakeMove(knightMove);
+
+            Assert.AreEqual(board.GetLegalMoves(king).Count, 0, "The black king should not be able to make a move after only a knight has moved.");
+
+            Move pawnMove = new Move(board.PieceAt(new Vector2Int(6, 1)), new Vector2Int(6, 2));
+            Move bishopMove = new Move(board.PieceAt(new Vector2Int(5, 0)), new Vector2Int(7, 2));
+            board.MakeMove(pawnMove);
+            board.MakeMove(bishopMove);
+
+            Assert.AreEqual(board.GetLegalMoves(king).Count, 1, "There is a problem if castling isn't possible for the black king.");
+        }
     }
 }
